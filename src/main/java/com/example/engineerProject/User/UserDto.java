b7a47ee745c94673c98e4c15b87ac33ba81f4d59
Wojtech99 <wojteknab@gmail.com>
2022-11-30 16:35:34 +0100

    create UserDto Class

---
+++ src/main/java/com/example/engineerProject/User/UserDto.java
@@ -0,0 +1,124 @@
+package com.example.engineerProject.User;
+
+import com.example.engineerProject.Announcement.Announcement;
+import com.example.engineerProject.Article.Article;
+
+import java.util.Objects;
+import java.util.Set;
+
+class UserDto {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private Integer phoneNumber;
+    private String email;
+    private String password;
+    private Set<Announcement> announcements;
+    private Set<Article> articles;
+    private Role role;
+
+    public UserDto(Long id, String firstName, String lastName, Integer phoneNumber,
+                   String email, String password, Set<Announcement> announcements, Set<Article> articles, Role role) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+        this.password = password;
+        this.announcements = announcements;
+        this.articles = articles;
+        this.role = role;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Integer getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(Integer phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Set<Announcement> getAnnouncements() {
+        return announcements;
+    }
+
+    public void setAnnouncements(Set<Announcement> announcements) {
+        this.announcements = announcements;
+    }
+
+    public Set<Article> getArticles() {
+        return articles;
+    }
+
+    public void setArticles(Set<Article> articles) {
+        this.articles = articles;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UserDto userDto = (UserDto) o;
+        return Objects.equals(id, userDto.id) && Objects.equals(firstName, userDto.firstName) &&
+                Objects.equals(lastName, userDto.lastName) &&
+                Objects.equals(phoneNumber, userDto.phoneNumber) &&
+                Objects.equals(email, userDto.email) &&
+                Objects.equals(password, userDto.password) &&
+                Objects.equals(announcements, userDto.announcements) &&
+                Objects.equals(articles, userDto.articles) && role == userDto.role;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, firstName, lastName, phoneNumber, email,
+                password, announcements, articles, role);
+    }
+}
