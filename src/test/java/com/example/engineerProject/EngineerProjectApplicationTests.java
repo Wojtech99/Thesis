26589565111fe2e6bafee01abdb4f8b2cd20a817
Wojtech99 <wojteknab@gmail.com>
2022-11-30 16:33:19 +0100

    Initial commit

---
+++ src/test/java/com/example/engineerProject/EngineerProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.engineerProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EngineerProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
