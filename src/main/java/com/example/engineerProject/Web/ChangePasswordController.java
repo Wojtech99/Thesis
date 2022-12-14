7aa69a476f50b9f4a18309148ef9cff1e6fb4bea
Wojtech99 <wojteknab@gmail.com>
2022-12-14 19:02:29 +0100

    create change password function in project

--- src/main/java/com/example/engineerProject/Web/ChangePasswordController.java
+++ src/main/java/com/example/engineerProject/Web/ChangePasswordController.java
@@ -2,8 +2,13 @@ package com.example.engineerProject.Web;
 
 import com.example.engineerProject.User.AppUserService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Objects;
 
 @Controller
 public class ChangePasswordController {
@@ -13,7 +18,15 @@ public class ChangePasswordController {
         this.userService = userService;
     }
 
-    @PostMapping("/change-password")
+    @GetMapping("/change-password")
+    String changePasswordForm(Model model) {
+        model.addAttribute("newPassword", "");
+        model.addAttribute("repeatNewPassword", "");
+
+        return "change-password";
+    }
+
+    @PostMapping("/change-password/save")
     String changePassword(@RequestParam("newPassword") String newPassword) {
         userService.changePassword(newPassword);
 
