dbc70e806c84b5e7acef89f21ddeec69ef7c1536
Wojtech99 <wojteknab@gmail.com>
2022-12-21 18:15:47 +0100

    Create agreement controller and html pages

---
+++ src/main/java/com/example/engineerProject/Agreement/AgreementController.java
@@ -0,0 +1,94 @@
+package com.example.engineerProject.Agreement;
+
+import jakarta.annotation.Resource;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.NoSuchElementException;
+import java.util.Set;
+
+@Controller
+public class AgreementController {
+
+    private final AgreementService agreementService;
+
+    public AgreementController(AgreementService agreementService) {
+        this.agreementService = agreementService;
+    }
+
+    /**
+     * show new agreement form
+     * @param model Model
+     * @return String
+     */
+    @GetMapping("/add-agreement")
+    String addAgreementPage(Model model) {
+        return "new-agreement-form";
+    }
+
+    /**
+     * save new pdf agreement
+     * @param multipartFile MultipartFile
+     * @return String
+     */
+    @PostMapping("/add-agreement/save")
+    String saveAgreement(@RequestParam("pdfFile") MultipartFile multipartFile) {
+
+        try {
+            agreementService.saveAgreement(multipartFile);
+        } catch (IOException exception) {
+            throw new NoSuchElementException();
+        }
+
+        return "redirect:/add-agreement";
+    }
+
+    /**
+     * show all agreements
+     * @param model Model
+     * @return String
+     */
+    @GetMapping("/show-agreements")
+    String showAllAgreements(Model model) {
+        Set<AgreementDto> agreements = agreementService.showAllAgreements();
+
+        model.addAttribute("agreements", agreements);
+
+        return "agreements-list";
+    }
+
+    /**
+     * delete agreement
+     * @param agreementId Long
+     * @return String
+     */
+    @RequestMapping(
+            value = "/show-agreements/delete/{id}",
+            method = {RequestMethod.DELETE, RequestMethod.GET}
+    )
+    String deleteAgreement(@PathVariable("id") Long agreementId) {
+        agreementService.deleteAgreement(agreementId);
+        return"redirect:/show-agreements";
+    }
+
+    @GetMapping("/show-agreements/download/{id}")
+    ResponseEntity<ByteArrayResource> downloadAgreement(@PathVariable("id") Long agreementId) {
+        AgreementDto agreementDto = agreementService.getAgreement(agreementId);
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType(agreementDto.getType()))
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment;filename=\""
+                        + agreementDto.getName() + "\"")
+                .body(new ByteArrayResource(agreementDto.getBytes()));
+    }
+}
